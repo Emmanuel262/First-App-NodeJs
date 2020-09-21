dd381d93e9feb715c590b5f6e2b707bcf94789ae
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2020-09-21 20:24:51 +0200

    api key solving prob

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: '164946218657611',
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
